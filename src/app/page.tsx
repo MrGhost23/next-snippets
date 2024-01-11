56a420b6170287f977eb2c3607e4e4a686fce7a7
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-01-11 18:57:30 +0200

    some ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
-import Image from "next/image";
+import { db } from "@/db";
 
-export default function Home() {
-  return <div>Home Page</div>;
+export default async function Home() {
+  const snippets = await db.snippet.findMany({});
+
+  return (
+    <div className="flex flex-wrap">
+      {snippets.map((snippet) => (
+        <div key={snippet.id} className="w-full sm:w-1/2 md:w-1/4 p-4">
+          <div className="flex flex-col bg-white rounded-lg shadow-md hover:shadow-lg overflow-hidden">
+            <div className="p-6">
+              <p className="text-xl font-semibold text-gray-900">
+                {snippet.title}
+              </p>
+              <pre className="mt-3 text-base text-gray-500">{snippet.code}</pre>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
